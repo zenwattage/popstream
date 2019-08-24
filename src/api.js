45ceb1464c01d159dc6e6f04460b49b1a0119a83
Scott H <zenwattage@gmail.com>
2019-08-23 21:56:29 -0700

    mass push - working app - TODO: more styling

---
+++ src/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+let api = axios.create({
+    headers: {
+        'Client-ID': 'ftr2xhjp8cuh2az7ek9xfjimc9ql6v'
+    }
+});
+
+export default api;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Games from './components/Games';
+import Header from './components/Header';
+import Stream from './components/Streams';
+import GameStreams from './components/GameStreams';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+import './styles.css';
+function App() {
+    return (
+        <Router>
+            <div className="App container-fluid">
+                <Header />
+                <Route exact path='/' component={Games} />
+                <Route exact path='/top-streams' component={Stream} />
+                <Route path='/game/:id' component={GameStreams} />
+            </div>
+        </Router>
+    );
+}
+
+const rootElement = document.getElementById("root");
+
+
+ReactDOM.render(<App />, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
